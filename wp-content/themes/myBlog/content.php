629af252e2d7400284927cae5dd45047c6d1b58c
Matt Larson <matt.a.larson@gmail.com>
2017-07-20 17:19:29 -0500

    added comments

--- wp-content/themes/myBlog/content.php
+++ wp-content/themes/myBlog/content.php
@@ -5,3 +5,8 @@
  <?php the_excerpt(); ?>
 
 </div><!-- /.blog-post -->
+
+<a href="<?php comments_link(); ?>">
+	<?php
+	printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
+</a>
--- wp-content/themes/myBlog/single.php
+++ wp-content/themes/myBlog/single.php
@@ -4,9 +4,14 @@
 		<div class="col-sm-12">
 
 			<?php
-				if ( have_posts() ) : while ( have_posts() ) : the_post();
-					get_template_part( 'content-single', get_post_format() );
-				endwhile; endif;
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
+				get_template_part( 'content-single', get_post_format() );
+
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; endif;
 			?>
 
 		</div> <!-- /.col -->
